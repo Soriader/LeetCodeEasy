3099e70abfe14350dd735ea3ef1ffae59964afba
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-05-01 22:31:30 +0200

    Initial commit

---
+++ LeetCodeEasy/TwoSumTask.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCodeEasy;
+
+public class TwoSumTask
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        var firstIndex = 0;
+        var secondIndex = 0;
+
+        while (secondIndex == 0)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[firstIndex] + nums[i] == target && i != firstIndex)
+                {
+                    secondIndex = i;
+                }
+            }
+
+            if (secondIndex == 0)
+            {
+                firstIndex++;
+            }
+            else
+            {
+                break;
+            }
+
+        }
+        
+        return new int[] {firstIndex, secondIndex};
+    }
+}
+//https://leetcode.com/problems/two-sum/description/
---
+++ LeetCodeEasyTest/TwoSumTest.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCodeEasyTest;
+
+using LeetCodeEasy;
+
+public class TwoSumTest
+{
+    [Test]
+    public void TwoSums()
+    {
+        Assert.AreEqual(new int[]{0,1}, TwoSumTask.TwoSum(new int[]{2,7,11,15}, 9));
+        Assert.AreEqual(new int[]{1,2}, TwoSumTask.TwoSum(new int[]{3,2,4}, 6));
+        Assert.AreEqual(new int[]{0,1}, TwoSumTask.TwoSum(new int[]{3,3}, 6));
+    }
+}
