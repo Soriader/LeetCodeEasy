e78cc149fb137a2e7e8ae2ace4871fd1f1d19992
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-05-17 14:33:01 +0200

    [~] Task: Search Insert Position

---
+++ LeetCodeEasy/SearchInsertPositionTask.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeEasy;
+
+public class SearchInsertPositionTask
+{
+    public static int SearchInsert(int[] nums, int target) 
+    {
+        if (nums.Contains(target))
+        {
+            return Array.IndexOf(nums, target);
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] < target 
+                && i != nums.Length - 1
+                && nums[i + 1] > target 
+                || i == nums.Length - 1 && nums[i] < target
+                )
+            {
+                return i + 1;
+            }
+
+        }
+        
+        return 0;
+    }
+}
+//https://leetcode.com/problems/search-insert-position/description/
---
+++ LeetCodeEasyTest/SearchInsertPositionTest.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCodeEasyTest;
+
+using LeetCodeEasy;
+
+public class SearchInsertPositionTest
+{
+    [Test]
+    public void SearchInsertPosition()
+    {
+        Assert.AreEqual(2, SearchInsertPositionTask.SearchInsert(new int[]{1,3,5,6}, 5));
+        Assert.AreEqual(4, SearchInsertPositionTask.SearchInsert(new int[]{1,3,5,6}, 7));
+        Assert.AreEqual(1, SearchInsertPositionTask.SearchInsert(new int[]{1,3,5,6}, 2));
+        Assert.AreEqual(0, SearchInsertPositionTask.SearchInsert(new int[]{1,3,5,6}, 0));
+
+    }
+}
