d951e93175c65e02e06aad35c1f277682da1aa98
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-05-18 14:24:00 +0200

    [~] Task: Plus One

---
+++ LeetCodeEasy/PlusOneTask.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCodeEasy;
+
+public class PlusOneTask
+{
+    public static int[] PlusOne(int[] digits)
+    {
+        for (int i = digits.Length - 1; i >= 0; i--)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+        
+            digits[i] = 0;
+        }
+        
+        int[] result = new int[digits.Length + 1];
+        result[0] = 1;
+        
+        return result;
+    }
+}
+//https://leetcode.com/problems/plus-one/description/
+
+//This result is still ok but not for numbers which have more value than inr properties
+/*public static int[] PlusOne(int[] digits)
+{
+    var boxForNumber = "";
+    foreach (int i in digits)
+    {
+        boxForNumber += i.ToString();
+    }
+        
+    var numbers = (int.Parse(boxForNumber) + 1).ToString().ToCharArray();
+
+    var result = new List<int>();
+        
+    foreach (char c in numbers)
+    {
+        result.Add(int.Parse(c.ToString()));
+    }
+        
+    return result.ToArray();
+}*/
+
---
+++ LeetCodeEasyTest/PlusOneTest.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCodeEasyTest;
+
+using LeetCodeEasy;
+
+public class PlusOneTest
+{
+    [Test]
+    public void PlusOne()
+    {
+        Assert.AreEqual(new int[]{1,2,4}, PlusOneTask.PlusOne(new int[]{1,2,3}));
+        Assert.AreEqual(new int[]{4,3,2,2}, PlusOneTask.PlusOne(new int[]{4,3,2,1}));
+        Assert.AreEqual(new int[]{1,0}, PlusOneTask.PlusOne(new int[]{9}));
+    }
+}
