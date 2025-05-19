b45c29428cbc434245cb44c03875bc2f7294afba
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-05-19 20:16:59 +0200

    [~] Task: Remove Duplicates from Sorted List

---
+++ LeetCodeEasy/RemoveDuplicatesFromSortedListTask.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeEasy;
+
+public class RemoveDuplicatesFromSortedListTask
+{
+    public ListNode DeleteDuplicates(ListNode head) 
+    {
+        if (head == null)
+        {           
+            return null;
+        }
+        
+        ListNode current = head;
+
+        while (current.next != null)
+        {
+            if (current.val == current.next.val)
+            {
+                current.next = current.next.next;
+            }
+            else
+            {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+}
+//https://leetcode.com/problems/remove-duplicates-from-sorted-list/
