61e5b65d2a92ea8055a14441fafe948bc1554a34
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-05-19 18:42:43 +0200

    [~]Task: Climbing stairs

---
+++ LeetCodeEasy/ClimbingStairsTask.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeEasy;
+
+public class ClimbingStairsTask
+{
+    public static int ClimbStairs(int n)
+    {
+        if (n == 1)
+        {
+            return 1;
+        }
+    
+        int first = 1; 
+        int second = 2; 
+    
+        for (int i = 3; i <= n; i++)
+        {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+    
+        return second;
+    }
+}
+//https://leetcode.com/problems/climbing-stairs/
---
+++ LeetCodeEasyTest/ClimbingStairsTest.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCodeEasyTest;
+
+using LeetCodeEasy;
+
+public class ClimbingStairsTest
+{
+    [Test]
+    public void ClimbingStairs()
+    {
+        Assert.AreEqual(2, ClimbingStairsTask.ClimbStairs(2));
+        Assert.AreEqual(3, ClimbingStairsTask.ClimbStairs(3));
+        Assert.AreEqual(1, ClimbingStairsTask.ClimbStairs(1));
+        Assert.AreEqual(5, ClimbingStairsTask.ClimbStairs(4));
+    }
+}
