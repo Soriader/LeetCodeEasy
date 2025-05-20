61fbc68fe3d2c36cfd53475b2949b40682ec830d
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-05-20 14:25:39 +0200

    [~] Task: Same Tree

---
+++ LeetCodeEasy/SameTreeTask.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCodeEasy;
+
+public class SameTreeTask
+{
+    public bool IsSameTree(TreeNode p, TreeNode q) 
+    {
+        if (p == null && q == null)
+        {
+            return true;
+        }   
+        
+        if (p == null || q == null)
+        {
+            return false;
+        }    
+        return p.val == q.val 
+               && IsSameTree(p.left, q.left) 
+               && IsSameTree(p.right, q.right);
+        
+    }
+}
+//https://leetcode.com/problems/same-tree/
