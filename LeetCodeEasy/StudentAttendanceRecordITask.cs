c501d4d7339822621939e56858ffbd1d578fbf6d
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-05-22 18:30:14 +0200

    [~] Task: Student Attendance Record I

---
+++ LeetCodeEasy/StudentAttendanceRecordITask.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeEasy;
+
+public class StudentAttendanceRecordITask
+{
+    public static bool CheckRecord(string s)
+    {
+        int isAbsent = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == 'A')
+            {
+                isAbsent++;
+                if (isAbsent >= 2)
+                {
+                    return false;
+                }
+            }
+
+            if (i + 2 < s.Length 
+                && s[i] == 'L' 
+                && s[i + 1] == 'L' 
+                && s[i + 2] == 'L')
+            {
+                return false;
+            }
+        }
+        
+        
+        return true;
+    }
+}
+//https://leetcode.com/problems/student-attendance-record-i/description/
---
+++ LeetCodeEasyTest/StudentAttendanceRecordITest.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCodeEasyTest;
+
+using LeetCodeEasy;
+
+public class StudentAttendanceRecordITest
+{
+    [Test]
+    public void StudentAttendanceRecordI()
+    {
+        Assert.AreEqual(true, StudentAttendanceRecordITask.CheckRecord("PPALLP"));
+        Assert.AreEqual(false, StudentAttendanceRecordITask.CheckRecord("PPALLL"));
+        Assert.AreEqual(true, StudentAttendanceRecordITask.CheckRecord("ALL"));
+    }
+}
