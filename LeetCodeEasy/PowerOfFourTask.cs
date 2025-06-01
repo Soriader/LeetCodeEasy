6c28bc2b4de2eade32a7c6b10abac71706172140
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-01 19:27:11 +0200

    [~] Task: Power of four

---
+++ LeetCodeEasy/PowerOfFourTask.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCodeEasy;
+
+public class PowerOfFourTask
+{
+    public static bool IsPowerOfFour(int n)
+    {
+        if (n <= 0)
+        {
+            return false;
+        }
+    
+        double log = Math.Log(n, 4);
+        return log == Math.Floor(log);
+    }
+}
+//https://leetcode.com/problems/power-of-four/description/
---
+++ LeetCodeEasyTest/PowerOfFourTest.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCodeEasyTest;
+
+using LeetCodeEasy;
+
+public class PowerOfFourTest
+{
+    [Test]
+    public void PowerOfFour()
+    {
+        Assert.AreEqual(true, PowerOfFourTask.IsPowerOfFour(16));
+        Assert.AreEqual(true, PowerOfFourTask.IsPowerOfFour(1));
+        Assert.AreEqual(false, PowerOfFourTask.IsPowerOfFour(5));
+        Assert.AreEqual(false, PowerOfFourTask.IsPowerOfFour(1162261466));
+    }
+}
