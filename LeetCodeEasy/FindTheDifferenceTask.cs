5f0cd885d600b3149696d015eb4cfc91f3b09a0a
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-08 16:35:09 +0200

    [~] Task: Find the Difference

---
+++ LeetCodeEasy/FindTheDifferenceTask.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCodeEasy;
+
+public class FindTheDifferenceTask
+{
+    public static char FindTheDifference(string s, string t)
+    {
+        var sCount = s
+            .GroupBy(c => c)
+            .ToDictionary(g => g.Key, g => g.Count());
+        
+        var tCount = t
+            .GroupBy(c => c)
+            .ToDictionary(g => g.Key, g => g.Count());
+
+        foreach (var kvp in tCount)
+        {
+            if (!sCount.ContainsKey(kvp.Key))
+            {
+                return kvp.Key;
+            }
+        }
+
+        foreach (var kvp in sCount)
+        {
+            if (tCount[kvp.Key] > kvp.Value)
+            {
+                return kvp.Key;
+            }
+        }
+
+        return t[0];
+    }
+}
+//https://leetcode.com/problems/find-the-difference/description/
---
+++ LeetCodeEasyTest/FindTheDifferenceTest.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCodeEasyTest;
+
+using LeetCodeEasy;
+
+public class FindTheDifferenceTest
+{
+    [Test]
+    public void ShortestCompWord()
+    {
+        Assert.AreEqual('e', FindTheDifferenceTask.FindTheDifference("abcd", "abcde"));
+    }
+}
