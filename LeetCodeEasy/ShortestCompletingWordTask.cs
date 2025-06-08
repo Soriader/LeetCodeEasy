6c725efdd1b6c01c4403eac209ad82b5007c37c7
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-08 13:03:43 +0200

    [~] Task: Shortest Completing Word

---
+++ LeetCodeEasy/ShortestCompletingWordTask.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCodeEasy;
+
+public class ShortestCompletingWordTask
+{
+    public static string ShortestCompletingWord(string licensePlate, string[] words) 
+    {
+        var cleanedLicensePlate = new string(licensePlate
+            .ToLower()
+            .Where(c => char.IsLetter(c))
+            .ToArray());
+        var boxForResults = new List<string>();
+        
+
+        var licenseCounts = cleanedLicensePlate
+            .GroupBy(c => c)
+            .ToDictionary(g => g.Key, g => g.Count());
+
+        foreach (var word in words)
+        {
+            var wordCounts = word
+                .GroupBy(c => c)
+                .ToDictionary(g => g.Key, g => g.Count());
+
+            bool isValid = licenseCounts.All(kv => 
+                wordCounts.ContainsKey(kv.Key) && 
+                wordCounts[kv.Key] >= kv.Value);
+    
+            if (isValid)
+            {
+                boxForResults.Add(word);
+            }
+        }
+        
+        return boxForResults.OrderBy(word => word.Length).First();    
+    }
+}
+//https://leetcode.com/problems/shortest-completing-word/description/
---
+++ LeetCodeEasyTest/ShortestCompletingWordTest.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCodeEasyTest;
+
+using LeetCodeEasy;
+
+public class ShortestCompletingWordTest
+{
+    [Test]
+    public void ShortestCompWord()
+    {
+        Assert.AreEqual("pest", ShortestCompletingWordTask.ShortestCompletingWord("1s3 456",new string[]{"looks","pest","stew","show"}));
+    }
+}
