186727750ead21d494bce954cc4c79c8350f8c06
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-08 12:19:32 +0200

    [~] Task: Largest Number At Least Twice of Others

---
+++ LeetCodeEasy/LargestNumberAtLeastTwiceOfOthersTask.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCodeEasy;
+
+public class LargestNumberAtLeastTwiceOfOthersTask
+{
+    public static int DominantIndex(int[] nums) 
+    {
+        var maxValue = nums.Max();
+        var withoutMaxNum = nums.Where(x => x != maxValue).ToArray();
+        
+        if (withoutMaxNum.All(x => maxValue >= 2 * x))
+        {
+            return Array.IndexOf(nums, maxValue);
+        }
+
+        return -1;
+    }
+}
+//https://leetcode.com/problems/largest-number-at-least-twice-of-others/description/
---
+++ LeetCodeEasyTest/LargestNumberAtLeastTwiceOfOthersTest.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCodeEasyTest;
+
+using LeetCodeEasy;
+
+public class LargestNumberAtLeastTwiceOfOthersTest
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.AreEqual(-1, LargestNumberAtLeastTwiceOfOthersTask.DominantIndex(new int[]{1,2,3,4}));
+        Assert.AreEqual(1, LargestNumberAtLeastTwiceOfOthersTask.DominantIndex(new int[]{3,6,1,0}));
+    }
+}
