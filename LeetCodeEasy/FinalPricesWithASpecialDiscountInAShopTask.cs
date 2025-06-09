5deeaeefa1c5b5229552ab4957c1661dfa6797c8
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-09 14:48:03 +0200

    [~] Task: Final Prices With a Special Discount in a Shop

---
+++ LeetCodeEasy/FinalPricesWithASpecialDiscountInAShopTask.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeEasy;
+
+public class FinalPricesWithASpecialDiscountInAShopTask
+{
+    public static int[] FinalPrices(int[] prices)
+    {
+        int[] result = new int[prices.Length];
+    
+        for (int i = 0; i < prices.Length; i++)
+        {
+            result[i] = prices[i];
+        
+            for (int j = i + 1; j < prices.Length; j++)
+            {
+                if (prices[j] <= prices[i])
+                {
+                    result[i] = prices[i] - prices[j];
+                    break;
+                }
+            }
+        }
+    
+        return result;
+    }
+}
+//https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/description/
---
+++ LeetCodeEasyTest/FinalPricesWithASpecialDiscountInAShopTest.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCodeEasyTest;
+
+using LeetCodeEasy;
+
+public class FinalPricesWithASpecialDiscountInAShopTest
+{
+    [Test]
+    public void FinalPricesWithASpecialDiscountInAShop()
+    {
+        var input = new int[] { 8, 4, 6, 2, 3 };
+        var expected = new int[] { 4,2,4,2,3 };
+        Assert.AreEqual(expected, FinalPricesWithASpecialDiscountInAShopTask.FinalPrices(input));
+    }
+    
+}
