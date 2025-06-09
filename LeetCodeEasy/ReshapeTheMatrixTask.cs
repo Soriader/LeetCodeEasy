daaebd0e8e99dde6834d547c6e553bc5f2d70548
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-09 13:33:05 +0200

    [~] Task: Reshape the Matrix

---
+++ LeetCodeEasy/ReshapeTheMatrixTask.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCodeEasy;
+
+public class ReshapeTheMatrixTask
+{
+    public static int[][] MatrixReshape(int[][] mat, int r, int c)
+    {
+        int m = mat.Length;
+        int n = mat[0].Length;
+    
+        if (m * n != r * c) 
+        {
+            return mat;
+        }
+    
+        int[][] result = new int[r][];
+        
+        for (int i = 0; i < r; i++) 
+        {
+            result[i] = new int[c];
+        }
+
+        int row = 0; 
+        int col = 0;
+        
+        for (int i = 0; i < m; i++) 
+        {
+            for (int j = 0; j < n; j++) 
+            {
+                result[row][col] = mat[i][j];
+                col++;
+                
+                if (col == c) 
+                {
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+    
+        return result;
+    }
+}
+//https://leetcode.com/problems/reshape-the-matrix/description/
---
+++ LeetCodeEasyTest/ReshapeTheMatrixTest.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCodeEasyTest;
+
+using LeetCodeEasy;
+
+public class ReshapeTheMatrixTest
+{
+        [Test]
+        public void ReshapeTheMatrix()
+        {
+                var input = new int[][] { new int[] { 1, 2 }, new int[] { 3, 4 } };
+                int r = 1, c = 4;
+                var expected = new int[][] { new int[] { 1, 2, 3, 4 } };
+
+                var result = ReshapeTheMatrixTask.MatrixReshape(input, r, c);
+
+                Assert.AreEqual(expected, result);
+        }
+}
