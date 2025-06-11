1466a91ae4dd2b8509c952304d95f52c366746da
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-11 13:05:49 +0200

    [~] Task: Fair Candy Swap

---
+++ LeetCodeEasy/FairCandySwapTask.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeEasy;
+
+public class FairCandySwapTask
+{
+    public static int[] FairCandySwap(int[] aliceSizes, int[] bobSizes) 
+    {
+        var sumA = aliceSizes.Sum(); 
+        var sumB = bobSizes.Sum();
+        var result = new int[aliceSizes.Length];
+
+        for (int i = 0; i < aliceSizes.Length; i++)
+        {
+            for (int j = 0; j < bobSizes.Length; j++)
+            {
+                if (sumA - sumB == 2*(aliceSizes[i] - bobSizes[j]))
+                {
+                    result =[aliceSizes[i], bobSizes[j]]; 
+                }
+            }
+        }
+        
+        return result;
+    }
+}
+//https://leetcode.com/problems/fair-candy-swap/description/
---
+++ LeetCodeEasyTest/FairCandySwapTest.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCodeEasyTest;
+
+using LeetCodeEasy;
+
+public class FairCandySwapTest
+{
+
+    [Test]
+    public void FairCandySwap()
+    {
+        Assert.AreEqual(new int[]{1,2}, FairCandySwapTask.FairCandySwap(new int[]{1,1}, new int[]{2,2}));
+    }
+    
+}
