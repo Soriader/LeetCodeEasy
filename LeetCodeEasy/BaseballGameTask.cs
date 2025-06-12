9457aee6e01de4509d5350ad3bdcf5d13c889aeb
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-12 19:45:29 +0200

    [~] Task: Baseball Game

---
+++ LeetCodeEasy/BaseballGameTask.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCodeEasy;
+
+public class BaseballGameTask
+{
+    private static readonly Dictionary<char, Action<List<int>>> optionsForResult = new Dictionary<char, Action<List<int>>>
+    {
+        {'+', (scores) => {
+            if (scores.Count >= 2)
+            {
+                int newScore = scores[scores.Count - 1] + scores[scores.Count - 2];
+                scores.Add(newScore);
+            }
+        }},
+        {'D', (scores) => {
+            if (scores.Count >= 1)
+            {
+                int newScore = scores[scores.Count - 1] * 2;
+                scores.Add(newScore);
+            }
+        }},
+        {'C', (scores) => {
+            if (scores.Count >= 1)
+            {
+                scores.RemoveAt(scores.Count - 1);
+            }
+        }}
+    };
+    
+    public static int CalPoints(string[] operations) 
+    {
+        var boxForResult = new List<int>();
+        
+        foreach (string op in operations)
+        {
+            if (int.TryParse(op, out int number))
+            {
+                boxForResult.Add(number);
+            }
+            else if (op.Length == 1 && optionsForResult.ContainsKey(op[0]))
+            {
+                optionsForResult[op[0]](boxForResult);
+            }
+        }
+        
+        return boxForResult.Sum();
+    }
+}
+//https://leetcode.com/problems/baseball-game/description/
---
+++ LeetCodeEasyTest/BaseballGameTest.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCodeEasyTest;
+
+using LeetCodeEasy;
+
+public class BaseballGameTest
+{
+    [Test]
+    public void BaseballGame()
+    {
+        Assert.AreEqual(30,BaseballGameTask.CalPoints(new string[]{"5","2","C","D","+"}));
+        Assert.AreEqual(27,BaseballGameTask.CalPoints(new string[]{"5","-2","4","C","D","9","+","+"}));
+    }
+}
