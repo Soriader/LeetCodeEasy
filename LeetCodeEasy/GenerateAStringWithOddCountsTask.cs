2923e0f866e2795d8472a5d42a55733dcd31cfd3
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-13 15:20:29 +0200

    [~] Task: Generate a String With Characters That Have Odd Counts

---
+++ LeetCodeEasy/GenerateAStringWithOddCountsTask.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeEasy;
+
+public class GenerateAStringWithOddCountsTask
+{
+    public static string GenerateTheString(int n) 
+    {
+        if (n % 2 == 1)  
+        {
+            return string.Concat(Enumerable.Repeat("a", n));
+        }
+        else  
+        {
+            return string.Concat(Enumerable.Repeat("a", n - 1)) + "b";
+        }
+    }
+}
+//https://leetcode.com/problems/generate-a-string-with-characters-that-have-odd-counts/description/
