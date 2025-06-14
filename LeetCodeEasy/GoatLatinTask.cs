e86fa343e6fa38af8c7d7a2ff9ed78cba7e926fd
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-14 22:08:29 +0200

    [~] Task: Goat Latin [.] Added 'LeetCodeEasy' using to 'GlobalUsings' in test project

---
+++ LeetCodeEasy/GoatLatinTask.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeEasy;
+
+public class GoatLatinTask
+{
+    public static string ToGoatLatin(string sentence) 
+    {
+        var vovels = "aeiouAEIOU";
+        var prtsOFSentence = sentence.Split(' ');
+        var addA = "a";
+        string result = "";
+
+        foreach (var word in prtsOFSentence)
+        {
+            if (vovels.Contains(word[0]))
+            {
+                result += word + "ma" + addA + " ";
+            }
+            else if (word.Length == 0) 
+            {
+            } 
+            else if (word.Length == 1) 
+            {
+                result += word + "ma" + addA + " ";
+            } 
+            else 
+            {
+                result += word.Substring(1) + word[0] + "ma" + addA + " ";
+            }
+            
+            addA += "a";
+        }
+
+        return result.TrimEnd(' ');
+    }
+}
+//https://leetcode.com/problems/goat-latin/description/
---
+++ LeetCodeEasyTest/GoatLatinTest.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCodeEasyTest;
+
+public class GoatLatinTest
+{
+    [Test]
+    public void GoatLatin()
+    {
+        Assert.AreEqual("Imaa peaksmaaa oatGmaaaa atinLmaaaaa", GoatLatinTask.ToGoatLatin("I speak Goat Latin"));
+    }
+}
