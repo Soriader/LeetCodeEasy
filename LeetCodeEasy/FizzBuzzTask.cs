7a48df27f970b768013a5ac9ae1c9e538687a72f
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-15 16:20:17 +0200

    [~] Task: Fizz Buzz

---
+++ LeetCodeEasy/FizzBuzzTask.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCodeEasy;
+
+public class FizzBuzzTask
+{
+    public static IList<string> FizzBuzz(int n) 
+    {
+        var result = new List<string>();
+        
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                result.Add("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                result.Add("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                result.Add("Buzz");
+            }
+            else
+            {
+                result.Add(i.ToString());
+            }
+        }
+        
+        return result;
+    }
+}
+//https://leetcode.com/problems/fizz-buzz/description/
---
+++ LeetCodeEasyTest/FizzBuzzTest.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCodeEasyTest;
+
+public class FizzBuzzTest
+{
+    [Test]
+    public void FizzBuzz()
+    {
+        Assert.AreEqual(new List<string>(){"1","2","Fizz"}, FizzBuzzTask.FizzBuzz(3));
+        Assert.AreEqual(new List<string>(){"1","2","Fizz","4","Buzz"}, FizzBuzzTask.FizzBuzz(5));
+        Assert.AreEqual(new List<string>(){"1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"},
+            FizzBuzzTask.FizzBuzz(15));
+    }
+}
