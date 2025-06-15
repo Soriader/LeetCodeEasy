6cb342c9575d9c8959d16f584776540807b8967c
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-15 16:57:39 +0200

    [~] Task: Add Strings

---
+++ LeetCodeEasy/AddStringsTask.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace LeetCodeEasy;
+
+public class AddStringsTask
+{
+    public static string AddStrings(string num1, string num2) 
+    {
+        int i = num1.Length - 1;
+        int j = num2.Length - 1;
+        int carry = 0;
+        StringBuilder result = new StringBuilder();
+    
+        while (i >= 0 || j >= 0 || carry > 0) 
+        {
+            int digit1 = i >= 0 ? num1[i--] - '0' : 0;
+            int digit2 = j >= 0 ? num2[j--] - '0' : 0;
+            int sum = digit1 + digit2 + carry;
+            carry = sum / 10;
+            result.Append(sum % 10);
+        }
+    
+        var charArray = result.ToString().ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
+//https://leetcode.com/problems/add-strings/description/
---
+++ LeetCodeEasyTest/AddStringsTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeEasyTest;
+
+public class AddStringsTest
+{
+    [Test]
+    public void AddStrings()
+    {
+        Assert.AreEqual("134", AddStringsTask.AddStrings("11", "123"));
+        Assert.AreEqual("533", AddStringsTask.AddStrings("456", "77"));
+    }
+}
