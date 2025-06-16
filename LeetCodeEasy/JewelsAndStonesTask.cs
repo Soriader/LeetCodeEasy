ee55c4b86a4aeeeed080aa6258fe5c499c10c5dc
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-16 12:22:49 +0200

    [~] Task: Jewels and Stones

---
+++ LeetCodeEasy/JewelsAndStonesTask.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeEasy;
+
+public class JewelsAndStonesTask
+{
+    public static int NumJewelsInStones(string jewels, string stones) 
+    {
+     
+        var jewelsList = jewels.ToCharArray();
+        var result = 0;
+
+        foreach (var jew in stones.ToCharArray())
+        {
+            if (jewelsList.Contains(jew))
+            {
+                result++;
+            }
+        }
+        return result;
+    }
+}
+//https://leetcode.com/problems/jewels-and-stones/description/
---
+++ LeetCodeEasyTest/JewelsAndStonesTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeEasyTest;
+
+public class JewelsAndStonesTest
+{
+    [Test]
+    public void JewelsAndStones()
+    {
+        Assert.AreEqual(3, JewelsAndStonesTask.NumJewelsInStones("aA", "aAAbbbb"));
+        Assert.AreEqual(0, JewelsAndStonesTask.NumJewelsInStones("z", "ZZ"));
+    }
+}
