b8ba0e721c3f47a60d6ec73ad8d6a5493a4f826f
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-17 09:09:17 +0200

    [~] Task: Kids With the Greatest Number of Candies

---
+++ LeetCodeEasy/KidsWithTheGreatestNumberOfCandiesTask.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeEasy;
+
+public class KidsWithTheGreatestNumberOfCandiesTask
+{
+    public static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        var result = new List<bool>();
+
+        for (int i = 0; i < candies.Length; i++)
+        {
+            int extraCandiesToAdd = extraCandies + candies[i];
+            
+            if (candies.All(x => x <= extraCandiesToAdd))
+            {
+                result.Add(true);
+            }
+            else
+            {
+                result.Add(false);
+            }
+        }
+        
+        return result;
+    }
+}
+//https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/description/
---
+++ LeetCodeEasyTest/KidsWithTheGreatestNumberOfCandiesTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeEasyTest;
+
+public class KidsWithTheGreatestNumberOfCandiesTest
+{
+    [Test]
+    public void KidsWithTheGreatestNumberOfCandies()
+    {
+        Assert.AreEqual(new List<bool>(){true,true,true,false,true}, KidsWithTheGreatestNumberOfCandiesTask.KidsWithCandies(new int[]{2,3,5,1,3}, 3));
+        Assert.AreEqual(new List<bool>(){true,false,true}, KidsWithTheGreatestNumberOfCandiesTask.KidsWithCandies(new int[]{12, 1, 12}, 10));
+    }
+}
