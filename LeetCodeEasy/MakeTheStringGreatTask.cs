62483e0ef4271db45baa319a54026c35a1b88967
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-18 12:02:25 +0200

    [~] Task: Make The String Great

---
+++ LeetCodeEasy/MakeTheStringGreatTask.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeEasy;
+
+public class MakeTheStringGreatTask
+{
+    public static string MakeGood(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+    
+        foreach (char currentChar in s)
+        {
+            if (stack.Count > 0 && 
+                Math.Abs(stack.Peek() - currentChar) == 32)
+            {
+                stack.Pop();  
+            }
+            else
+            {
+                stack.Push(currentChar);
+            }
+        }
+    
+        char[] result = stack.ToArray();
+        Array.Reverse(result);
+        return new string(result);
+    }
+}
+//https://leetcode.com/problems/make-the-string-great/description/
---
+++ LeetCodeEasyTest/MakeTheStringGreatTest.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCodeEasyTest;
+
+public class MakeTheStringGreatTest
+{
+    [Test]
+    public void MakeTheStringGreat()
+    {
+        Assert.AreEqual("leetcode", MakeTheStringGreatTask.MakeGood("leEeetcode"));
+        Assert.AreEqual("", MakeTheStringGreatTask.MakeGood("abBAcC"));
+        Assert.AreEqual("s", MakeTheStringGreatTask.MakeGood("s"));
+        Assert.AreEqual("mC", MakeTheStringGreatTask.MakeGood("mC"));
+    }
+}
