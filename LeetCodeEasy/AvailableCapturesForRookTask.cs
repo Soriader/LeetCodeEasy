376d1c8c322037e07a520e3cd106a965e13484e2
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-19 12:07:57 +0200

    [~] Task: Available Captures for Rook

---
+++ LeetCodeEasy/AvailableCapturesForRookTask.cs
@@ -0,0 +1,63 @@
+﻿namespace LeetCodeEasy;
+
+public class AvailableCapturesForRookTask
+{
+    public static int NumRookCaptures(char[][] board)
+    {
+        int rookRow = -1;
+        int rookCol = -1;
+        
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                if (board[i][j] == 'R')
+                {
+                    rookRow = i;
+                    rookCol = j;
+                    break;
+                }
+            }
+            if (rookRow != -1) break;
+        }
+
+        int attackedPawn = 0;
+
+        int[] directions = { -1, 1 }; 
+
+        foreach (int dir in directions)
+        {
+            for (int col = rookCol + dir; col >= 0 && col < 8; col += dir)
+            {
+                if (board[rookRow][col] == 'p')
+                {
+                    attackedPawn++;
+                    break;
+                }
+                if (board[rookRow][col] != '.')
+                {
+                    break;
+                }
+            }
+        }
+
+        foreach (int dir in directions)
+        {
+            for (int row = rookRow + dir; row >= 0 && row < 8; row += dir)
+            {
+                if (board[row][rookCol] == 'p')
+                {
+                    attackedPawn++;
+                    break;
+                }
+                if (board[row][rookCol] != '.')
+                {
+                    break;
+                }
+            }
+        }
+
+        return attackedPawn;
+    }
+}
+//https://leetcode.com/problems/available-captures-for-rook/description/
---
+++ LeetCodeEasyTest/AvailableCapturesForRookTest.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCodeEasyTest;
+
+public class AvailableCapturesForRookTest
+{
+    [Test]
+    public void AvailableCapturesForRookTestOne()
+    {
+        char[][] board = new char[][]
+        {
+            new char[] { '.', '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', 'p', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', 'R', '.', '.', '.', 'p' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', 'p', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.', '.' }
+        };
+        
+        Assert.AreEqual(3, AvailableCapturesForRookTask.NumRookCaptures(board));
+    }
+    
+    [Test]
+    public void AvailableCapturesForRookTestTwo()
+    {
+        char[][] board = new char[][]
+        {
+            new char[] { '.', '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', 'p', 'p', 'p', 'p', 'p', '.', '.' },
+            new char[] { '.', 'p', 'p', 'B', 'p', 'p', '.', '.' },
+            new char[] { '.', 'p', 'B', 'R', 'B', 'p', '.', '.' },
+            new char[] { '.', 'p', 'p', 'B', 'p', 'p', '.', '.' },
+            new char[] { '.', 'p', 'p', 'p', 'p', 'p', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.', '.' }
+        };
+        
+        Assert.AreEqual(0, AvailableCapturesForRookTask.NumRookCaptures(board));
+    }
+}
