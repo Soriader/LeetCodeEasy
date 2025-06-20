d6c844cc9f719f53b7c0ed33526fa44ce679bfbe
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-20 17:48:00 +0200

    [~] Task: Most Common Word

---
+++ LeetCodeEasy/MostCommonWordTask.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCodeEasy;
+
+public class MostCommonWordTask
+{
+    public static string MostCommonWord(string paragraph, string[] banned) 
+    {
+        string check = new string(paragraph
+            .ToLower()
+            .Select(c => char.IsLetter(c) ? c : ' ')
+            .ToArray());
+
+        string[] words = check.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+        var countBox = new Dictionary<string, int>();
+        var bannedLower = banned.Select(b => b.ToLower()).ToArray();
+
+        foreach (var word in words)
+        {
+            if (bannedLower.Contains(word))
+            {
+                continue;
+            }
+            if (countBox.ContainsKey(word))
+            {
+                countBox[word]++;
+            }
+            else
+            {
+                countBox[word] = 1;
+            }        
+        }
+
+        return countBox.OrderByDescending(p => p.Value).First().Key;
+    }
+
+}
+//https://leetcode.com/problems/most-common-word/description/
---
+++ LeetCodeEasyTest/MostCommonWordTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeEasyTest;
+
+public class MostCommonWordTest
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.AreEqual("ball", MostCommonWordTask.MostCommonWord( "Bob hit a ball, the hit BALL flew far after it was hit.", new string[]{"hit"}));
+        Assert.AreEqual("a", MostCommonWordTask.MostCommonWord( "a.", new string[]{}));
+    }
+}
