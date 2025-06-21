43fcb0e71d9e7c8f6c03b1cce10393ab4c9bf14d
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-21 13:02:38 +0200

    [~] Task: Minimum Cost to Move Chips to The Same Position

---
+++ LeetCodeEasyTest/MinimumCostToMoveChipsTest.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCodeEasyTest;
+
+public class MinimumCostToMoveChipsTest
+{
+    [Test]
+    public void MinCostToMoveChips()
+    {
+        Assert.AreEqual(1, MinimumCostToMoveChipsTask.MinCostToMoveChips(new int[] { 1, 2, 3 }));
+        Assert.AreEqual(1, MinimumCostToMoveChipsTask.MinCostToMoveChips(new int[] { 1,1000000000 }));
+        Assert.AreEqual(2, MinimumCostToMoveChipsTask.MinCostToMoveChips(new int[] { 2,2,2,3,3 }));
+    }
+}
