bb25b30f87821a6c251dd240badcdfacdd4ca090
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-22 12:32:13 +0200

    [~] Task: Divide a String Into Groups of Size k

---
+++ LeetCodeEasy/DivideAStringIntoGroupsOfSizeKTask.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeEasy;
+
+public class DivideAStringIntoGroupsOfSizeKTask
+{
+    public static string[] DivideString(string s, int k, char fill) 
+    {
+        if (s is null)
+        {
+            return null;
+        }
+        
+        var result = new List<string>();
+        var part = "";
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            part += s[i];
+            
+            if (part.Length == k)
+            {
+                result.Add(part);
+                part = "";
+            }
+            else if (i == s.Length - 1 && part.Length != k)
+            {
+                part += String.Concat(Enumerable.Repeat($"{fill}", k - part.Length));
+                result.Add(part);
+                break;
+            }
+        }
+        
+        
+        return result.ToArray();
+    }
+}
+//https://leetcode.com/problems/divide-a-string-into-groups-of-size-k/description/?envType=daily-question&envId=2025-06-22
---
+++ LeetCodeEasyTest/DivideAStringIntoGroupsOfSizeKTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeEasyTest;
+
+public class DivideAStringIntoGroupsOfSizeKTest
+{
+    [Test]
+    public void DivideAStringIntoGroupsOfSizeK()
+    {
+        Assert.AreEqual(new string[]{"abc","def","ghi"},DivideAStringIntoGroupsOfSizeKTask.DivideString("abcdefghi", 3, 'x'));
+        Assert.AreEqual(new string[]{"abc","def","ghi","jxx"},DivideAStringIntoGroupsOfSizeKTask.DivideString("abcdefghij", 3, 'x'));
+    }
+}
