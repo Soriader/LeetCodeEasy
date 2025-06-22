c7b9f3dd80381e3b3cea15566d89e19fb511b08b
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-22 12:51:55 +0200

    [~] Task: Unique Number of Occurrences

---
+++ LeetCodeEasy/UniqueNumberOfOccurrencesTask.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeEasy;
+
+public class UniqueNumberOfOccurrencesTask
+{
+    public static bool UniqueOccurrences(int[] arr) 
+    {
+        var countTheNumbers = new Dictionary<int, int>();
+
+        foreach (var number in arr)
+        {
+            if (countTheNumbers.ContainsKey(number))
+            {
+                countTheNumbers[number]++;
+            }
+            else
+            {
+                countTheNumbers.Add(number, 1);
+            }
+        }
+
+        var occurrences = countTheNumbers.Values;
+        return occurrences.Distinct().Count() == occurrences.Count();
+    }
+}
+//https://leetcode.com/problems/unique-number-of-occurrences/
---
+++ LeetCodeEasyTest/UniqueNumberOfOccurrencesTest.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCodeEasyTest;
+
+public class UniqueNumberOfOccurrencesTest
+{
+    [Test]
+    public void UniqueNumberOfOccurrences()
+    {
+        Assert.AreEqual(true, UniqueNumberOfOccurrencesTask.UniqueOccurrences(new int[]{1,2,2,1,1,3}));
+        Assert.AreEqual(true, UniqueNumberOfOccurrencesTask.UniqueOccurrences(new int[]{-3,0,1,-3,1,1,1,-3,10,0}));
+        Assert.AreEqual(false, UniqueNumberOfOccurrencesTask.UniqueOccurrences(new int[]{1,2}));
+    }
+}
