d495cc17d3388cf767fd5b312dfb24cdc7d0b9fc
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-23 17:47:15 +0200

    [~] Task: The K Weakest Rows in a Matrix

---
+++ LeetCodeEasy/TheKWeakestRowsInAMatrixTask.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCodeEasy;
+
+public class TheKWeakestRowsInAMatrixTask
+{
+    public static int[] KWeakestRows(int[][] mat, int k)
+    {
+        var soliderSummary = new Dictionary<int, int>();
+        int iterator = 0;
+
+        for (int i = 0; i < mat.Length; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < mat[i].Length; j++)
+            {
+                if (mat[i][j] == 1)
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            soliderSummary.Add(iterator, count);
+            iterator++;
+        }
+        
+        var result = new List<int>();
+        iterator = 0;
+
+        while (iterator != k)
+        {
+            var weakestRow = soliderSummary.OrderBy(x => x.Value).First();
+            result.Add(weakestRow.Key);
+    
+            soliderSummary.Remove(weakestRow.Key);
+            iterator++;
+        }
+        
+        return result.ToArray();
+    }
+}
+//https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/description/
---
+++ LeetCodeEasyTest/TheKWeakestRowsInAMatrixTest.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCodeEasyTest;
+
+public class TheKWeakestRowsInAMatrixTest
+{
+    [Test]
+    public void TheKWeakestRowsInAMatrix()
+    {
+        var mat = new int[][]
+        {
+            new int[] { 1,1,0,0,0}, 
+            new int[] { 1,1,1,1,0 },
+            new int[] { 1,0,0,0,0 },
+            new int[] { 1,1,0,0,0 },
+            new int[] { 1,1,1,1,1 }
+        };
+        
+        Assert.AreEqual(new int[]{2,0,3}, TheKWeakestRowsInAMatrixTask.KWeakestRows(mat, 3));
+    }
+}
