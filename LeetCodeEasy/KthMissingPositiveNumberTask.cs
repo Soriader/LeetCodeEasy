4413d156fcbca814bd740167bd17bab21f2cd17f
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-23 18:01:12 +0200

    [~] Task: Kth Missing Positive Number

---
+++ LeetCodeEasy/KthMissingPositiveNumberTask.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeEasy;
+
+public class KthMissingPositiveNumberTask
+{
+    public static int FindKthPositive(int[] arr, int k) 
+    {
+        var missingNumbers = new List<int>();
+        int currentNumber = 1;
+        int index = 0;
+
+        while (missingNumbers.Count < k)
+        {
+            if (index < arr.Length && arr[index] == currentNumber)
+            {
+                index++;
+            }
+            else
+            {
+                missingNumbers.Add(currentNumber);
+            }
+            currentNumber++;
+        }
+
+        return missingNumbers[k - 1];
+    }
+}
+//https://leetcode.com/problems/kth-missing-positive-number/description/
---
+++ LeetCodeEasyTest/KthMissingPositiveNumberTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeEasyTest;
+
+public class KthMissingPositiveNumberTest
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.AreEqual(9, KthMissingPositiveNumberTask.FindKthPositive(new int[]{2,3,4,7,11}, 5));
+        Assert.AreEqual(6, KthMissingPositiveNumberTask.FindKthPositive(new int[]{1,2,3,4}, 2));
+    }
+}
