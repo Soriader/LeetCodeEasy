bd9edd3d011f395af1c3895eb96964cc15e7dad4
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-24 18:03:00 +0200

    [~] Task: Find All K-Distant Indices in an Array

---
+++ LeetCodeEasy/FindAllKDistantIndicesInAnArrayTask.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeEasy;
+
+public class FindAllKDistantIndicesInAnArrayTask
+{
+    public static IList<int> FindKDistantIndices(int[] nums, int key, int k) 
+    {
+        var result = new List<int>();
+        var keyBox = new List<int>();
+
+        for (int j = 0; j < nums.Length; j++)
+        {
+            if (nums[j] == key)
+            {
+                keyBox.Add(j);
+            }
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            foreach (int j in keyBox)
+            {
+                if (Math.Abs(i - j) <= k)
+                {
+                    result.Add(i);
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
+//https://leetcode.com/problems/find-all-k-distant-indices-in-an-array/description/?envType=daily-question&envId=2025-06-24
---
+++ LeetCodeEasyTest/FindAllKDistantIndicesInAnArrayTest.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCodeEasyTest;
+
+public class FindAllKDistantIndicesInAnArrayTest
+{
+    [Test]
+    public void FindAllKDistantIndicesInAnArray()
+    {
+        Assert.AreEqual(new[]{1,2,3,4,5,6}, FindAllKDistantIndicesInAnArrayTask.FindKDistantIndices(new[] { 3,4,9,1,3,9,5 }, 9, 1));
+    }
+}
