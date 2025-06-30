241dbf396b2c527555f2bcab3d3ca608867b3890
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-30 18:11:20 +0200

    [~] Task: Longest Harmonious Subsequence

---
+++ LeetCodeEasy/LongestHarmoniousSubsequenceTask.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCodeEasy;
+
+public class LongestHarmoniousSubsequenceTask
+{
+    public static int FindLHS(int[] nums) 
+    {
+        var countTheNumbers = new Dictionary<int, int>();
+        int result = 0;
+        
+        foreach (var num in nums)
+        {
+            if (countTheNumbers.ContainsKey(num))
+            {
+                countTheNumbers[num]++;
+            }
+            else
+            {
+                countTheNumbers.Add(num, 1);
+            }
+        }
+
+        foreach (var num in countTheNumbers.Keys.ToList())
+        {
+            if (countTheNumbers.ContainsKey(num + 1))
+            {
+                int currentLength = countTheNumbers[num] + countTheNumbers[num + 1];
+                if (currentLength > result)
+                {
+                    result = currentLength;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
+//https://leetcode.com/problems/longest-harmonious-subsequence/description/?envType=daily-question&envId=2025-06-30
---
+++ LeetCodeEasyTest/LongestHarmoniousSubsequenceTest.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCodeEasyTest;
+
+public class LongestHarmoniousSubsequenceTest
+{
+    [Test]
+    public void LongestHarmoniousSubsequence()
+    {
+        Assert.AreEqual(5, LongestHarmoniousSubsequenceTask.FindLHS(new int[]{1,3,2,2,5,2,3,7}));
+        Assert.AreEqual(2, LongestHarmoniousSubsequenceTask.FindLHS(new int[]{1,2,3,4}));
+        Assert.AreEqual(0, LongestHarmoniousSubsequenceTask.FindLHS(new int[]{1,1,1,1}));
+    }
+}
