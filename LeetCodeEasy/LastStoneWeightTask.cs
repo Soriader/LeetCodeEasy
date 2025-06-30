cd3923ec658adb401dbb4256aed8e39c87cbf0f9
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-30 18:33:03 +0200

    [~] Task: Last Stone Weight

---
+++ LeetCodeEasy/LastStoneWeightTask.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeEasy;
+
+public class LastStoneWeightTask
+{
+    public static int LastStoneWeight(int[] stones) 
+    {
+        var stonesList = stones.ToList();
+        
+        while (stonesList.Count > 1)
+        {
+            stonesList.Sort((a, b) => b.CompareTo(a));
+            int y = stonesList[0];
+            int x = stonesList[1];
+    
+            stonesList.RemoveAt(0); 
+            stonesList.RemoveAt(0);
+    
+            if (x != y)
+            {
+                stonesList.Add(y - x);
+            }
+            
+        }
+        
+        return stonesList.Count == 0 ? 0 : stonesList[0];
+    }
+}
+//https://leetcode.com/problems/last-stone-weight/description/
---
+++ LeetCodeEasyTest/LastStoneWeightTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeEasyTest;
+
+public class LastStoneWeightTest
+{
+    [Test]
+    public void LastStone()
+    {
+        Assert.AreEqual(1, LastStoneWeightTask.LastStoneWeight(new int[] { 2,7,4,1,8,1}));
+        Assert.AreEqual(1, LastStoneWeightTask.LastStoneWeight(new int[] {1}));
+    }
+}
