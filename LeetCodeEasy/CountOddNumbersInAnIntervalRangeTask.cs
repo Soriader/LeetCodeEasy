3ad6031611679fbb517581241857a06ca2382814
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-02 21:48:44 +0200

    [~] Task: Count Odd Numbers in an Interval Range

---
+++ LeetCodeEasy/CountOddNumbersInAnIntervalRangeTask.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeEasy;
+
+public class CountOddNumbersInAnIntervalRangeTask
+{
+    public static int CountOdds(int low, int high)
+    {
+        
+        int totalNumbers = high - low + 1;
+    
+        if (totalNumbers % 2 == 0)
+        {
+            return totalNumbers / 2;
+        }
+        else
+        {
+            return (low % 2 != 0) ? (totalNumbers / 2 + 1) : (totalNumbers / 2);
+        }
+    }
+}
+//https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/description/
---
+++ LeetCodeEasyTest/CountOddNumbersInAnIntervalRangeTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeEasyTest;
+
+public class CountOddNumbersInAnIntervalRangeTest
+{
+    [Test]
+    public void CountOddNumbersInAnIntervalRange()
+    {
+        Assert.AreEqual(3, CountOddNumbersInAnIntervalRangeTask.CountOdds(3, 7));
+        Assert.AreEqual(1, CountOddNumbersInAnIntervalRangeTask.CountOdds(8, 10));
+    }
+}
