feea44923d41a5f3899d50bf6537402b4541f7ad
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-02 21:41:48 +0200

    [~] Task: Number of Days Between Two Dates

---
+++ LeetCodeEasy/NumberOfDaysBetweenTwoDatesTask.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCodeEasy;
+
+public class NumberOfDaysBetweenTwoDatesTask
+{
+    public static int DaysBetweenDates(string date1, string date2)
+    {
+        var firstDate = DateTime.Parse(date1);
+        var secondDate = DateTime.Parse(date2);
+        
+        var difference = secondDate - firstDate;
+        
+        return Math.Abs(difference.Days);
+    }
+}
+//https://leetcode.com/problems/number-of-days-between-two-dates/
---
+++ LeetCodeEasyTest/NumberOfDaysBetweenTwoDatesTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeEasyTest;
+
+public class NumberOfDaysBetweenTwoDatesTest
+{
+    [Test]
+    public void NumberOfDaysBetweenTwoDates()
+    {
+        Assert.AreEqual(1, NumberOfDaysBetweenTwoDatesTask.DaysBetweenDates("2019-06-29", "2019-06-30"));
+        Assert.AreEqual(15, NumberOfDaysBetweenTwoDatesTask.DaysBetweenDates("2020-01-15", "2019-12-31"));
+    }
+}
