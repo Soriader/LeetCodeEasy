c0a3c4d81e503ff3a4a69343bc26f4c9c6953cd6
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-03 21:09:55 +0200

    [~] Task: Sum of All Odd Length Subarrays

---
+++ LeetCodeEasy/SumOfAllOddLengthSubarraysTask.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeEasy;
+
+public class SumOfAllOddLengthSubarraysTask
+{
+    public static int SumOddLengthSubarrays(int[] arr)
+    {
+        int result = 0;
+        for (int i = 0; i < arr.Length; i++) 
+        {
+            for (int l = 1; i + l <= arr.Length; l += 2) 
+            {
+                for (int j = i; j < i + l; j++) 
+                {
+                    result += arr[j];
+                }
+            }
+        }
+        return result;
+    }
+}
+//https://leetcode.com/problems/sum-of-all-odd-length-subarrays/description/
---
+++ LeetCodeEasyTest/SumOfAllOddLengthSubarraysTest.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCodeEasyTest;
+
+public class SumOfAllOddLengthSubarraysTest
+{
+    [Test]
+    public void SumOfAllOddLengthSubarrays()
+    {
+        Assert.AreEqual(58, SumOfAllOddLengthSubarraysTask.SumOddLengthSubarrays(new int[]{1,4,2,5,3}));
+        Assert.AreEqual(3, SumOfAllOddLengthSubarraysTask.SumOddLengthSubarrays(new int[]{1,2}));
+        Assert.AreEqual(66, SumOfAllOddLengthSubarraysTask.SumOddLengthSubarrays(new int[]{10,11,12}));
+    }
+}
