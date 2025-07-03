03627bc5d92b620b335a28afd18bc93d1d710c27
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-03 16:22:46 +0200

    [~]Task: Longest Nice Substring

---
+++ LeetCodeEasy/LongestNiceSubstringTask.cs
@@ -0,0 +1,92 @@
+﻿namespace LeetCodeEasy;
+
+public class LongestNiceSubstringTask
+{
+    public static string LongestNiceSubstring(string s)
+    {
+        string result = "";
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            for (int j = i + 1; j <= s.Length; j++)
+            {
+                string substring = s.Substring(i, j - i);
+                if (IsNice(substring) && substring.Length > result.Length)
+                {
+                    result = substring;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static bool IsNice(string s)
+    {
+        foreach (char c in s)
+        {
+            if (char.IsUpper(c))
+            {
+                if (!s.Contains(char.ToLower(c)))
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (!s.Contains(char.ToUpper(c)))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+/*Input: s = "YazaAay"
+Output: "aAa"*/
+
+
+//https://leetcode.com/problems/longest-nice-substring/
+
+/*public static string LongestNiceSubstring(string s) 
+{
+    if (s.Length == 1)
+    {
+        return "";
+    }
+        
+    string result = "";
+    var boxForResult = "";
+    var lastElement = s.Last();
+        
+    foreach (var letter in s)
+    {
+        if (boxForResult.Length >= 1 
+            && char.IsUpper(letter)
+            && boxForResult.LastOrDefault(x => char.IsLower(x)) == char.ToLower(letter)) 
+        {
+            boxForResult += letter;
+        }
+        else if (boxForResult.Length >= 1                 
+                 && char.IsLower(letter) 
+                 && boxForResult.LastOrDefault(x => char.IsUpper(x)) == char.ToUpper(letter))
+        {
+            boxForResult += letter;
+        }
+
+        if (boxForResult == "" 
+            || boxForResult.LastOrDefault(x => char.IsUpper(x)) != char.ToUpper(letter)
+            || boxForResult.LastOrDefault(x => char.IsLower(x)) != char.ToLower(letter)
+            || letter == lastElement)
+        {
+            boxForResult = "";
+            boxForResult += letter;
+        }
+            
+            
+    }
+        
+    return result;
+}*/
---
+++ LeetCodeEasyTest/LongestNiceSubstringTest.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCodeEasyTest;
+
+public class LongestNiceSubstringTest
+{
+    [Test]
+    public void LongestNiceSubstring()
+    {
+        Assert.AreEqual("Bb", LongestNiceSubstringTask.LongestNiceSubstring("Bb"));
+        Assert.AreEqual("cChH", LongestNiceSubstringTask.LongestNiceSubstring("cChH"));
+        Assert.AreEqual("aAa", LongestNiceSubstringTask.LongestNiceSubstring("YazaAay"));
+        Assert.AreEqual("", LongestNiceSubstringTask.LongestNiceSubstring("c"));
+    }
+}
