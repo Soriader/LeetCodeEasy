14912099090bfbcba2922c01c27037badfdbca2f
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-03 20:34:30 +0200

    [~] Task: Find the K-th Character in String Game I

---
+++ LeetCodeEasy/FindTheKCharacterInGameITask.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeEasy;
+
+public class FindTheKCharacterInGameITask
+{
+    public static char KthCharacter(int k)
+    {
+        if (k == 1)
+        {
+            return 'a';
+        }
+        
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        var result = "ab";
+        while (result.Length < k)
+        {
+            var nextPart = "";
+            foreach (var letter in result)
+            {
+                int nextIndex = (alphabet.IndexOf(letter) + 1) % 26;
+                nextPart += alphabet[nextIndex];
+            }
+
+            result += nextPart;
+        }
+
+        return result[k - 1];
+    }
+}
+//https://leetcode.com/problems/find-the-k-th-character-in-string-game-i/?envType=daily-question&envId=2025-07-03
---
+++ LeetCodeEasyTest/FindTheKCharacterInGameITest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeEasyTest;
+
+public class FindTheKCharacterInGameITest
+{
+    [Test]
+    public void FindTheKCharacterInGameI()
+    {
+        Assert.AreEqual('b',FindTheKCharacterInGameITask.KthCharacter(5));
+        Assert.AreEqual('c',FindTheKCharacterInGameITask.KthCharacter(10));
+    }
+}
