cc600ac1f1cec6cbce7b9507bf6a926d6d651520
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-03 20:47:17 +0200

    [~] Task: Make Two Arrays Equal by Reversing Subarrays

---
+++ LeetCodeEasy/MakeTwoArraysEqualByReversingSubarraysTask.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCodeEasy;
+
+public class MakeTwoArraysEqualByReversingSubarraysTask
+{
+    public bool CanBeEqual(int[] target, int[] arr) 
+    {
+        return arr.OrderBy(x => x).SequenceEqual(target.OrderBy(x => x));
+    }
+}
+//https://leetcode.com/problems/make-two-arrays-equal-by-reversing-subarrays/
