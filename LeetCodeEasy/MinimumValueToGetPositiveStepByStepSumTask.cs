683c3c7577685667fab57ca71eaa45a0dffec477
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-05 20:10:14 +0200

    [~] Task: Minimum Value to Get Positive Step by Step Sum

---
+++ LeetCodeEasy/MinimumValueToGetPositiveStepByStepSumTask.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeEasy;
+
+public class MinimumValueToGetPositiveStepByStepSumTask
+{
+    public static int MinStartValue(int[] nums) 
+    { 
+        int minSum = 0;
+        int currentSum = 0;
+    
+        for (int i = 0; i < nums.Length; i++)
+        {
+            currentSum += nums[i];
+        
+            if (currentSum < minSum)
+            {
+                minSum = currentSum;
+            }
+        }
+    
+        return Math.Max(1, 1 - minSum);
+    }
+}
+//https://leetcode.com/problems/minimum-value-to-get-positive-step-by-step-sum/description/
---
+++ LeetCodeEasyTest/MinimumValueToGetPositiveStepByStepSumTest.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCodeEasyTest;
+
+public class MinimumValueToGetPositiveStepByStepSumTest
+{
+    [Test]
+    public void MinimumValueToGetPositiveStepByStepSum()
+    {
+        Assert.AreEqual(5, MinimumValueToGetPositiveStepByStepSumTask.MinStartValue(new int[]{-3,2,-3,4,2}));
+        Assert.AreEqual(1, MinimumValueToGetPositiveStepByStepSumTask.MinStartValue(new int[]{1,2}));
+        Assert.AreEqual(5, MinimumValueToGetPositiveStepByStepSumTask.MinStartValue(new int[]{1,-2,-3}));
+    }
+}
