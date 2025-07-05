fb4a4ea07cede83ad6e3173cb7d2508d0bccc57b
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-05 21:01:00 +0200

    [~] Task: Check If String Is a Prefix of Array

---
+++ LeetCodeEasy/CheckIfStringIsAPrefixOfArrayTask.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeEasy;
+
+public class CheckIfStringIsAPrefixOfArrayTask
+{
+    public static bool IsPrefixString(string s, string[] words)
+    {
+        var currentLength = 0;
+        var actualString = "";
+        foreach (var word in words)
+        {
+            currentLength += word.Length;
+            actualString += word;
+            
+            if (currentLength == s.Length && actualString == s)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
+//https://leetcode.com/problems/check-if-string-is-a-prefix-of-array/description/
---
+++ LeetCodeEasyTest/CheckIfStringIsAPrefixOfArrayTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeEasyTest;
+
+public class CheckIfStringIsAPrefixOfArrayTest
+{
+    [Test]
+    public void SecondLargestDigitInAString()
+    {
+        Assert.AreEqual(true, CheckIfStringIsAPrefixOfArrayTask.IsPrefixString("iloveleetcode", new string[]{"i","love","leetcode","apples"}));
+        Assert.AreEqual(false, CheckIfStringIsAPrefixOfArrayTask.IsPrefixString("iloveleetcode", new string[]{"apples","i","love","leetcode"}));
+    }
+}
