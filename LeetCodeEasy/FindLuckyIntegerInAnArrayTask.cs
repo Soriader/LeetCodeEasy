b52518ddd950432474342d5ce732a69d117def0f
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-05 19:16:13 +0200

    [~] Task: Find Lucky Integer in an Array

---
+++ LeetCodeEasy/FindLuckyIntegerInAnArrayTask.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeEasy;
+
+public class FindLuckyIntegerInAnArrayTask
+{
+    public static int FindLucky(int[] arr) 
+    {
+        
+        var countAllNumbers = new Dictionary<int, int>();
+    
+        foreach (var number in arr)
+        {
+            countAllNumbers[number] = countAllNumbers.TryGetValue(number, out int count) ? count + 1 : 1;
+        }
+
+        int result = -1;
+    
+        foreach (var (number, count) in countAllNumbers)
+        {
+            if (number == count && number > result)
+            {
+                result = number;
+            }
+        }
+
+        return result;
+    }
+}
+//https://leetcode.com/problems/find-lucky-integer-in-an-array/?envType=daily-question&envId=2025-07-05
---
+++ LeetCodeEasyTest/FindLuckyIntegerInAnArrayTest.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCodeEasyTest;
+
+public class FindLuckyIntegerInAnArrayTest
+{
+    [Test]
+    public void FindLuckyIntegerInAnArray()
+    {
+        Assert.AreEqual(2, FindLuckyIntegerInAnArrayTask.FindLucky(new int[]{2,2,3,4}));
+        Assert.AreEqual(3, FindLuckyIntegerInAnArrayTask.FindLucky(new int[]{1,2,2,3,3,3}));
+        Assert.AreEqual(-1, FindLuckyIntegerInAnArrayTask.FindLucky(new int[]{2,2,2,3,3}));
+    }
+}
