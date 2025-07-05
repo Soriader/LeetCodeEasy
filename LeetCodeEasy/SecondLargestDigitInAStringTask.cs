f327bb02b5579087c5e1759468b3e72765403109
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-05 20:27:44 +0200

    [~] Task: Second Largest Digit in a String

---
+++ LeetCodeEasy/SecondLargestDigitInAStringTask.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeEasy;
+
+public class SecondLargestDigitInAStringTask
+{
+    public static int SecondHighest(string s) 
+    {
+        List<int> digits = new List<int>();
+        
+        foreach (var num in s)
+        {
+            if (char.IsDigit(num) && !digits.Contains(num - '0'))
+            {
+                digits.Add(num - '0');
+            }
+        }
+        
+        if (digits.Count < 2)
+        {
+            return -1;
+        }
+        
+        return digits.OrderByDescending(d => d).Skip(1).First();
+    }
+}
+//https://leetcode.com/problems/second-largest-digit-in-a-string/description/
---
+++ LeetCodeEasyTest/SecondLargestDigitInAStringTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeEasyTest;
+
+public class SecondLargestDigitInAStringTest
+{
+    [Test]
+    public void SecondLargestDigitInAString()
+    {
+        Assert.AreEqual(2, SecondLargestDigitInAStringTask.SecondHighest("dfa12321afd"));
+        Assert.AreEqual(-1, SecondLargestDigitInAStringTask.SecondHighest("abc1111"));
+    }
+}
