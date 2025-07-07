007012d843b85794536657a9a97171e1c9b4940a
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-07 23:43:35 +0200

    [~] Task: Defuse the Bomb

---
+++ LeetCodeEasy/DefuseTheBombTask.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeEasy;
+
+public class DefuseTheBombTask
+{
+    public static int[] Decrypt(int[] code, int k) 
+    {
+        int n = code.Length;
+        var result = new int[n];
+        int step = k > 0 ? 1 : -1;
+        int absK = Math.Abs(k);
+
+        for (int i = 0; i < n; i++)
+        {
+            int sum = 0;
+            
+            for (int j = 1; j <= absK; j++)
+            {
+                int pos = (i + j * step + n) % n;
+                sum += code[pos];
+            }
+            
+            result[i] = sum;
+        }
+
+        return result;
+    }
+}
+//https://leetcode.com/problems/defuse-the-bomb/description/
---
+++ LeetCodeEasyTest/DefuseTheBombTest.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCodeEasyTest;
+
+public class DefuseTheBombTest
+{
+    [Test]
+    public void Test()
+    {
+        Assert.AreEqual(new int[]{12,10,16,13}, DefuseTheBombTask.Decrypt(new int[]{5,7,1,4}, 3));
+        Assert.AreEqual(new int[]{12,5,6,13}, DefuseTheBombTask.Decrypt(new int[]{2,4,9,3}, -2));
+        Assert.AreEqual(new int[]{0,0,0,0}, DefuseTheBombTask.Decrypt(new int[]{2,4,9,3}, 0));
+    }
+}
