779478e2b713dbf22e3c782f664a03f1e14aa7b3
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-08 21:49:12 +0200

    [~] Task: Find Closest Number to Zero

---
+++ LeetCodeEasy/FindClosestNumberToZeroTask.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeEasy;
+
+public class FindClosestNumberToZeroTask
+{
+    public static int FindClosestNumber(int[] nums)
+    {
+        int result = nums[0];
+        
+        foreach (var num in nums)
+        {
+            if (Math.Abs(num) < Math.Abs(result))
+            {
+                result = num;
+            }
+            else if (Math.Abs(num) == Math.Abs(result) && num > result)
+            {
+                result = num;
+            }
+        }
+        
+        return result;
+    }
+}
+//https://leetcode.com/problems/find-closest-number-to-zero/description/
---
+++ LeetCodeEasyTest/FindClosestNumberToZeroTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeEasyTest;
+
+public class FindClosestNumberToZeroTest
+{
+    [Test]
+    public void FindClosestNumberToZero()
+    {
+        Assert.AreEqual(1, FindClosestNumberToZeroTask.FindClosestNumber(new int[]{-4,-2,1,4,8}));
+        Assert.AreEqual(1, FindClosestNumberToZeroTask.FindClosestNumber(new int[]{2,-1,1}));
+    }
+}
