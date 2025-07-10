3e2fe3ca735006f6ce84cf3ff890096792e0857f
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-10 22:23:03 +0200

    [~] Task: Number of Different Integers in a String

---
+++ LeetCodeEasy/NumberOfDifferentIntegersInAStringTask.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace LeetCodeEasy;
+
+public class NumberOfDifferentIntegersInAStringTask
+{
+    public static int NumDifferentIntegers(string word) 
+    {
+        var result = new List<string>();
+        var currentNumber = new StringBuilder();
+        
+        foreach (char c in word)
+        {
+            if (char.IsDigit(c))
+            {
+                currentNumber.Append(c); 
+            }
+            else if (currentNumber.Length > 0)
+            {
+                string trimmedNumber = TrimLeadingZeros(currentNumber.ToString());
+                
+                if (!ContainsNumber(result, trimmedNumber))
+                {
+                    result.Add(trimmedNumber);
+                }
+                
+                currentNumber.Clear(); 
+            }
+        }
+        
+        if (currentNumber.Length > 0)
+        {
+            string trimmedNumber = TrimLeadingZeros(currentNumber.ToString());
+            if (!ContainsNumber(result, trimmedNumber))
+            {
+                result.Add(trimmedNumber);
+            }
+        }
+        
+        return result.Count;
+    }
+    
+    private static string TrimLeadingZeros(string numStr)
+    {
+        int i = 0;
+        
+        while (i < numStr.Length - 1 && numStr[i] == '0') 
+        {
+            i++;
+        }
+        
+        return numStr.Substring(i);
+    }
+    
+    private static bool ContainsNumber(List<string> numbers, string numStr)
+    {
+        foreach (string num in numbers)
+        {
+            if (num == numStr)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+//https://leetcode.com/problems/number-of-different-integers-in-a-string/description/
---
+++ LeetCodeEasyTest/NumberOfDifferentIntegersInAStringTest.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCodeEasyTest;
+
+public class NumberOfDifferentIntegersInAStringTest
+{
+    [Test]
+    public void NumberOfDifferentIntegersInAString()
+    {
+        
+        Assert.AreEqual(3, NumberOfDifferentIntegersInAStringTask.NumDifferentIntegers("a123bc34d8ef34"));
+        Assert.AreEqual(2, NumberOfDifferentIntegersInAStringTask.NumDifferentIntegers("leet1234code234"));
+        Assert.AreEqual(1, NumberOfDifferentIntegersInAStringTask.NumDifferentIntegers("a1b01c001"));
+        
+    }
+}
