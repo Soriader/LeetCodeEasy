747cc4b66371034221bbf05f190996d8c209d4b0
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-13 15:18:57 +0200

    [~] Task: Three Divisors

---
+++ LeetCodeEasyTest/ThreeDivisorsTest.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCodeEasyTest;
+
+public class ThreeDivisorsTest
+{
+    [Test]
+    public void ThreeDivisors()
+    {
+        Assert.AreEqual(true, ThreeDivisorsTask.IsThree(4));
+        Assert.AreEqual(true, ThreeDivisorsTask.IsThree(529));
+        Assert.AreEqual(true, ThreeDivisorsTask.IsThree(961));
+        Assert.AreEqual(true, ThreeDivisorsTask.IsThree(49));
+        Assert.AreEqual(false, ThreeDivisorsTask.IsThree(2));
+        Assert.AreEqual(false, ThreeDivisorsTask.IsThree(50));
+        Assert.AreEqual(false, ThreeDivisorsTask.IsThree(962));
+    }
+}
