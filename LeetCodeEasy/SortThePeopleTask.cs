14a005d42d3b0c69e1adab39f5554c316507229e
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-15 19:58:15 +0200

    [~] Task: Sort the People

---
+++ LeetCodeEasy/SortThePeopleTask.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCodeEasy;
+
+public class SortThePeopleTask
+{
+    public static string[] SortPeople(string[] names, int[] heights) 
+    {
+        var people = names.Zip(heights, (name, height) => new { name, height });
+        
+        return people.OrderByDescending(p => p.height)
+            .Select(p => p.name)
+            .ToArray();
+    }
+}
+//https://leetcode.com/problems/sort-the-people/
---
+++ LeetCodeEasyTest/SortThePeopleTest.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCodeEasyTest;
+
+public class SortThePeopleTest
+{
+    [Test]
+    public void SortThePeople()
+    {
+        Assert.AreEqual(new string[]{"Mary","Emma","John"}, 
+            SortThePeopleTask.SortPeople(new string[]{"Mary","John","Emma"}, new int[]{180,165,170}));
+        
+        Assert.AreEqual(new string[]{"Bob","Alice","Bob"}, 
+            SortThePeopleTask.SortPeople(new string[]{"Alice","Bob","Bob"}, new int[]{155,185,150}));
+    }
+}
