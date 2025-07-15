51ba70a32a4c523f4feba5ec558bc6d615f6cd00
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-15 21:05:25 +0200

    [~] Task: Number of Unequal Triplets in Array

---
+++ LeetCodeEasy/NumberOfUnequalTripletsInArrayTask.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeEasy;
+
+public class NumberOfUnequalTripletsInArrayTask
+{
+    public static int UnequalTriplets(int[] nums)
+    {
+        int result = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                for (int k = j + 1; k < nums.Length; k++)
+                {
+                    if (nums[i] != nums[j] && nums[i] != nums[k] && nums[j] != nums[k]) 
+                    {
+                        result++;
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+}
+//https://leetcode.com/problems/number-of-unequal-triplets-in-array/description/
---
+++ LeetCodeEasyTest/NumberOfUnequalTripletsInArrayTest.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCodeEasyTest;
+
+public class NumberOfUnequalTripletsInArrayTest
+{
+    [Test]
+    public void NumberOfUnequalTripletsInArray()
+    {
+        Assert.AreEqual(3, NumberOfUnequalTripletsInArrayTask.UnequalTriplets(new[] { 4,4,2,4,3 }));
+        Assert.AreEqual(0, NumberOfUnequalTripletsInArrayTask.UnequalTriplets(new[] { 1,1,1,1,1 }));
+    }
+    
+}
