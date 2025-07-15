5d97a52a7ca1c7e2831b81b0d84cc4274ca644dc
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-15 17:52:55 +0200

    [~] Task: Valid Word

---
+++ LeetCodeEasy/ValidWordTask.cs
@@ -0,0 +1,64 @@
+﻿namespace LeetCodeEasy;
+
+public class ValidWordTask
+{
+    public static bool IsValid(string word) 
+    {
+        if(IsCorrectLength(word) 
+           && IsLetterOrDigit(word)
+           && IsContainsAVovel(word)
+           && IsContainsAConsonant(word))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static bool IsCorrectLength(string word)
+    {
+        return word.Length >= 3;
+    }
+
+    private static bool IsLetterOrDigit(string word)
+    {
+        foreach (char c in word)
+        {
+            if (!char.IsLetterOrDigit(c))
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    private static bool IsContainsAVovel(string word)
+    {
+        var vovels = "aeiouAEIOU";
+        
+        foreach (var c in word)
+        {
+            if (vovels.Contains(c.ToString()))
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    private static bool IsContainsAConsonant(string word)
+    {
+        foreach (var c in word)
+        {
+            if (char.IsLetter(c) && !IsContainsAVovel(c.ToString()))
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
+//https://leetcode.com/problems/valid-word/description/?envType=daily-question&envId=2025-07-15
---
+++ LeetCodeEasyTest/ValidWordTest.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCodeEasyTest;
+
+public class ValidWordTest
+{
+    [Test]
+    public void ValidWord()
+    {
+        Assert.IsTrue(ValidWordTask.IsValid("234Adas"));
+        Assert.IsTrue(ValidWordTask.IsValid("UuE6"));
+        Assert.IsFalse(ValidWordTask.IsValid("b3"));
+        Assert.IsFalse(ValidWordTask.IsValid("a3$e"));
+    }
+}
