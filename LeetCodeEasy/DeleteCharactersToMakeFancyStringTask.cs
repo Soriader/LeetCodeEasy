9ae747cf09abbc7eab1a49c6984d2d266c10f57a
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-21 17:51:55 +0200

    [~] Task: Delete Characters to Make Fancy String

---
+++ LeetCodeEasy/DeleteCharactersToMakeFancyStringTask.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace LeetCodeEasy;
+
+public class DeleteCharactersToMakeFancyStringTask
+{
+    public static string MakeFancyString(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+        {
+            return s;
+        }
+        
+        StringBuilder result = new StringBuilder();
+        int iterator = 1;
+        result.Append(s[0]);
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (s[i] == s[i - 1])
+            {
+                iterator++;
+            }
+            else
+            {
+                iterator = 1;
+            }
+
+            if (iterator < 3)
+            {
+                result.Append(s[i]);
+            }
+        }
+
+        return result.ToString();
+    }
+}
+//https://leetcode.com/problems/delete-characters-to-make-fancy-string/?envType=daily-question&envId=2025-07-21
---
+++ LeetCodeEasyTest/DeleteCharactersToMakeFancyStringTest.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCodeEasyTest;
+
+public class DeleteCharactersToMakeFancyStringTest
+{
+    [Test]
+    public void DeleteCharactersToMakeFancyString()
+    {
+        Assert.AreEqual("leetcode", DeleteCharactersToMakeFancyStringTask.MakeFancyString("leeetcode"));
+        Assert.AreEqual("aabaa", DeleteCharactersToMakeFancyStringTask.MakeFancyString("aaabaaaa"));
+        Assert.AreEqual("aab", DeleteCharactersToMakeFancyStringTask.MakeFancyString("aab"));
+    }
+}
