d0ff782441b47a5be17b14b3f03ab8732d40d620
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-21 17:58:40 +0200

    [~] Task : Smallest Even Multiple

---
+++ LeetCodeEasy/SmallestEvenMultipleTask.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeEasy;
+
+public class SmallestEvenMultipleTask
+{
+    public static int SmallestEvenMultiple(int n) 
+    {
+        if (n % 2 == 0)
+        {
+            return n;
+        }
+        else
+        {
+            return n * 2;
+        }
+    }
+}
+//https://leetcode.com/problems/smallest-even-multiple/description/
---
+++ LeetCodeEasyTest/SmallestEvenMultipleTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeEasyTest;
+
+public class SmallestEvenMultipleTest
+{
+    [Test]
+    public void SmallestEvenMultiple()
+    {
+        Assert.AreEqual(6, SmallestEvenMultipleTask.SmallestEvenMultiple(6));
+        Assert.AreEqual(10, SmallestEvenMultipleTask.SmallestEvenMultiple(5));
+    }
+}
