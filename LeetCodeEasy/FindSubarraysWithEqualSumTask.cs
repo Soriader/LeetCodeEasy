a88efdffa503667d3d0c1aa23a097cd0a2785b2f
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-21 18:20:53 +0200

    [~] Task: Find Subarrays With Equal Sum

---
+++ LeetCodeEasy/FindSubarraysWithEqualSumTask.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeEasy;
+
+public class FindSubarraysWithEqualSumTask
+{
+    public static bool FindSubarrays(int[] nums) 
+    {
+        var sums = new List<int>();
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            var sum = nums[i] + nums[i + 1];
+
+            if (sums.Contains(sum))
+            {
+                return true;
+            }
+            else
+            {
+                sums.Add(sum);
+            }
+        }
+        
+        return false;
+    }
+}
+//https://leetcode.com/problems/find-subarrays-with-equal-sum/
---
+++ LeetCodeEasyTest/FindSubarraysWithEqualSumTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeEasyTest;
+
+public class FindSubarraysWithEqualSumTest
+{
+    [Test]
+    public void FindSubarraysWithEqualSum()
+    {
+        Assert.AreEqual(false, FindSubarraysWithEqualSumTask.FindSubarrays(new int[] { 1, 3, 5, 7 }));
+        Assert.AreEqual(true, FindSubarraysWithEqualSumTask.FindSubarrays(new int[] { 4,2,4 }));
+    }
+}
