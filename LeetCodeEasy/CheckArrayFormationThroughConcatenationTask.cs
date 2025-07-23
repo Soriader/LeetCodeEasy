0cab532a86a63167cf682554f263db35db9c34c8
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-23 17:55:07 +0200

    [~] Task: Check Array Formation Through Concatenation

---
+++ LeetCodeEasy/CheckArrayFormationThroughConcatenationTask.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCodeEasy;
+
+public class CheckArrayFormationThroughConcatenationTask
+{
+    public static bool CanFormArray(int[] arr, int[][] pieces) 
+    {
+        Dictionary<int, int[]> map = new Dictionary<int, int[]>();
+        
+        foreach (var piece in pieces) 
+        {
+            map[piece[0]] = piece;
+        }
+
+        int i = 0;
+        while (i < arr.Length) 
+        {
+            if (!map.ContainsKey(arr[i])) 
+            {
+                return false;
+            }
+
+            int[] piece = map[arr[i]];
+            
+            for (int j = 0; j < piece.Length; j++) 
+            {
+                if (i >= arr.Length || arr[i] != piece[j]) 
+                {
+                    return false;
+                }
+                
+                i++;
+            }
+        }
+
+        return true;
+    }
+}
+//https://leetcode.com/problems/check-array-formation-through-concatenation/
---
+++ LeetCodeEasyTest/CheckArrayFormationThroughConcatenationTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeEasyTest;
+
+public class CheckArrayFormationThroughConcatenationTest
+{
+    [Test]
+    public void CheckArrayFormationThroughConcatenation()
+    {
+        Assert.AreEqual(true, CheckArrayFormationThroughConcatenationTask.CanFormArray(new int[]{15,88},new int[][]{[88],[15]}));
+        Assert.AreEqual(false, CheckArrayFormationThroughConcatenationTask.CanFormArray(new int[]{49,18,16},new int[][]{[16,18,49]}));
+    }
+}
