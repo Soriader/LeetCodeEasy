52e85e7c240e44325e7be6a2f5d6d998df655799
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-23 18:00:20 +0200

    [~] Task: Determine Color of a Chessboard Square

---
+++ LeetCodeEasy/DetermineColorOfAChessboardSquareTask.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCodeEasy;
+
+public class DetermineColorOfAChessboardSquareTask
+{
+    public static bool SquareIsWhite(string coordinates)
+    {
+        var firstIsBlack = "aceg";
+        var firstIsWhite = "bdfh";
+
+        if ((coordinates[1] - '0') % 2 == 0 && firstIsBlack.Contains(coordinates[0]) 
+            ||(coordinates[1] - '0') % 2 != 0 && firstIsWhite.Contains(coordinates[0]))
+        {
+            return true;
+        }
+        
+        return false;
+    }
+}
+//https://leetcode.com/problems/determine-color-of-a-chessboard-square/
---
+++ LeetCodeEasyTest/DetermineColorOfAChessboardSquareTest.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCodeEasyTest;
+
+public class DetermineColorOfAChessboardSquareTest
+{
+    [Test]
+    public void DetermineColorOfAChessboardSquare()
+    {
+        Assert.AreEqual(false, DetermineColorOfAChessboardSquareTask.SquareIsWhite("a1"));
+        Assert.AreEqual(true, DetermineColorOfAChessboardSquareTask.SquareIsWhite("a2"));
+        Assert.AreEqual(true, DetermineColorOfAChessboardSquareTask.SquareIsWhite("h3"));
+    }
+}
